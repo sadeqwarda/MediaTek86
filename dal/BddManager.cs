891f9e54eac12ed8f5917b1cf0faafb9a3775964
wmebrouk <wmebrouk@DESKTOP-5LLT2AA>
2026-05-17 22:54:56 +0200

    Ajout de la structure MVC

---
+++ dal/BddManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaTek86.dal
+{
+    internal class BddManager
+    {
+    }
+}
