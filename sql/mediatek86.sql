71ec2fdef76393355e8051a93854a5788eb8ca0a
wmebrouk <wmebrouk@DESKTOP-5LLT2AA>
2026-05-25 16:33:22 +0200

    màj  installateur finale màj base de données finale

--- sql/mediatek86.sql
+++ sql/mediatek86.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 23 mai 2026 à 16:56
+-- Généré le : lun. 25 mai 2026 à 14:31
 -- Version du serveur : 8.4.7
 -- Version de PHP : 8.3.28
 
@@ -17,27 +17,18 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE IF NOT EXISTS mediatek86;
-USE mediatek86;
-
-CREATE USER IF NOT EXISTS 'mediatekuser'@'localhost'
-IDENTIFIED BY 'mediatekpwd';
-
-GRANT SELECT, INSERT, UPDATE, DELETE
-ON mediatek86.*
-TO 'mediatekuser'@'localhost';
-
-FLUSH PRIVILEGES;
 --
 -- Base de données : `mediatek86`
 --
+CREATE DATABASE IF NOT EXISTS `mediatek86` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `mediatek86`;
+
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `absence`
 --
 
-DROP TABLE IF EXISTS `absence`;
 CREATE TABLE IF NOT EXISTS `absence` (
   `idpersonnel` int NOT NULL,
   `datedebut` datetime NOT NULL,
@@ -52,21 +43,19 @@ CREATE TABLE IF NOT EXISTS `absence` (
 --
 
 INSERT INTO `absence` (`idpersonnel`, `datedebut`, `datefin`, `idmotif`) VALUES
-(2, '2026-05-20 00:00:00', '2026-05-20 00:00:00', 4),
-(1, '2024-03-04 08:00:00', '2024-03-05 17:00:00', 2),
+(1, '2024-03-04 00:00:00', '2024-03-06 00:00:00', 2),
 (1, '2024-05-13 08:00:00', '2024-05-17 17:00:00', 1),
 (1, '2024-09-02 08:00:00', '2024-09-06 17:00:00', 4),
 (1, '2024-11-18 08:00:00', '2024-11-18 17:00:00', 3),
-(13, '2026-05-23 00:00:00', '2026-05-28 00:00:00', 3),
-(3, '2026-05-05 00:00:00', '2026-05-20 00:00:00', 1),
+(13, '2026-05-23 00:00:00', '2026-05-30 00:00:00', 3),
+(3, '2026-05-05 00:00:00', '2026-05-25 00:00:00', 1),
 (2, '2024-04-08 00:00:00', '2026-04-12 00:00:00', 2),
 (2, '2024-07-01 08:00:00', '2024-07-05 17:00:00', 1),
-(2, '2024-10-14 08:00:00', '2024-10-18 17:00:00', 4),
+(2, '2026-05-25 00:00:00', '2026-05-25 00:00:00', 4),
 (3, '2024-01-22 08:00:00', '2024-01-26 17:00:00', 1),
 (3, '2024-03-11 08:00:00', '2024-03-12 17:00:00', 2),
 (3, '2024-05-20 00:00:00', '2024-05-21 00:00:00', 3),
 (3, '2024-08-05 00:00:00', '2025-08-09 00:00:00', 1),
-(3, '2024-12-02 08:00:00', '2024-12-06 17:00:00', 4),
 (4, '2024-02-05 08:00:00', '2024-02-09 17:00:00', 1),
 (4, '2024-04-15 08:00:00', '2024-04-16 17:00:00', 2),
 (4, '2024-06-10 08:00:00', '2024-06-11 17:00:00', 3),
@@ -94,16 +83,15 @@ INSERT INTO `absence` (`idpersonnel`, `datedebut`, `datefin`, `idmotif`) VALUES
 (9, '2024-01-02 08:00:00', '2024-01-05 17:00:00', 1),
 (9, '2024-03-06 08:00:00', '2024-03-07 17:00:00', 2),
 (9, '2024-05-27 08:00:00', '2024-05-28 17:00:00', 3),
-(9, '2024-07-22 08:00:00', '2024-07-26 17:00:00', 1),
+(9, '2024-07-22 00:00:00', '2024-07-27 00:00:00', 1),
 (9, '2024-10-28 08:00:00', '2024-10-31 17:00:00', 4),
 (10, '2024-02-01 08:00:00', '2024-02-02 17:00:00', 2),
 (10, '2024-04-01 08:00:00', '2024-04-05 17:00:00', 1),
 (10, '2024-06-03 08:00:00', '2024-06-04 17:00:00', 3),
-(10, '2024-08-12 08:00:00', '2024-08-16 17:00:00', 1),
+(10, '2024-08-12 00:00:00', '2024-08-17 00:00:00', 2),
 (10, '2024-11-12 08:00:00', '2024-11-15 17:00:00', 4),
 (2, '2026-01-01 00:00:00', '2026-01-19 00:00:00', 2),
-(2, '2026-05-12 00:00:00', '2026-05-20 00:00:00', 4),
-(2, '2026-06-03 00:00:00', '2026-06-25 00:00:00', 4);
+(2, '2026-06-04 00:00:00', '2026-06-25 00:00:00', 4);
 
 -- --------------------------------------------------------
 
@@ -111,7 +99,6 @@ INSERT INTO `absence` (`idpersonnel`, `datedebut`, `datefin`, `idmotif`) VALUES
 -- Structure de la table `motif`
 --
 
-DROP TABLE IF EXISTS `motif`;
 CREATE TABLE IF NOT EXISTS `motif` (
   `idmotif` int NOT NULL AUTO_INCREMENT,
   `libelle` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -134,7 +121,6 @@ INSERT INTO `motif` (`idmotif`, `libelle`) VALUES
 -- Structure de la table `personnel`
 --
 
-DROP TABLE IF EXISTS `personnel`;
 CREATE TABLE IF NOT EXISTS `personnel` (
   `idpersonnel` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -144,23 +130,25 @@ CREATE TABLE IF NOT EXISTS `personnel` (
   `idservice` int NOT NULL,
   PRIMARY KEY (`idpersonnel`),
   KEY `idservice` (`idservice`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `personnel`
 --
 
 INSERT INTO `personnel` (`idpersonnel`, `nom`, `prenom`, `tel`, `mail`, `idservice`) VALUES
-(1, 'Martin', 'Jean', '0601010101', 'jean.martin@gmail.com', 1),
+(1, 'Martin', 'Jeanne', '0601010101', 'jean.martin@gmail.com', 1),
 (2, 'Bernard', 'jeanne', '0602020202', 'claire.bernard@hotmail.com', 1),
-(3, 'Dubois', 'Lucas', '0603030303', 'lucas.dubois@outlook.com', 2),
+(22, 'sadeq', 'maud', '0652525286', 'w.tse@2test.fr', 2),
 (4, 'Thomas', 'Emma', '0604040404', 'emma.thomas@gmail.com', 2),
 (5, 'Robert', 'Hugo', '0605050505', 'hugo.robert@hotmail.com', 3),
-(6, 'Richard', 'Manon', '0606060606', 'manon.richard@outlook.com', 3),
+(6, 'Richard', 'Manon', '0606060607', 'manon.richard@outlook.com', 3),
+(20, 'sade', 'warda', 'test', 'w.mail@test.fr', 2),
 (8, 'Durand', 'Léa', '0608080808', 'lea.durand@hotmail.com', 2),
 (9, 'Moreau', 'Tom', '0609090909', 'tom.moreau@outlook.com', 3),
-(10, 'Laurent', 'Inès', '0610101010', 'ines.laurent@gmail.com', 3),
-(13, 'Dubois', 'Luca', '0603030303', 'lucas.dubois@outlook.com', 2),
+(10, 'Laurent', 'Inès', '0610101018', 'ines.laurent@gmail.com', 3),
+(13, 'Dubois', 'Lucas', '0603030303', 'lucas.dubois@outlook.com', 2),
+(21, 'sade', 'wardo', 'test', 'w.mail@test.fr', 2),
 (15, 'karin', 'jean', '0652777777', 'karin@test.fr', 2),
 (17, 'Dubois', 'Lucas', '0603030303', 'lucas.dubois@outlook.com', 2),
 (19, 'sad', 'jamal', '0666655266', 'test@gmail.com', 1);
@@ -171,7 +159,6 @@ INSERT INTO `personnel` (`idpersonnel`, `nom`, `prenom`, `tel`, `mail`, `idservi
 -- Structure de la table `responsable`
 --
 
-DROP TABLE IF EXISTS `responsable`;
 CREATE TABLE IF NOT EXISTS `responsable` (
   `login` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `pwd` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL
@@ -190,7 +177,6 @@ INSERT INTO `responsable` (`login`, `pwd`) VALUES
 -- Structure de la table `service`
 --
 
-DROP TABLE IF EXISTS `service`;
 CREATE TABLE IF NOT EXISTS `service` (
   `idservice` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
